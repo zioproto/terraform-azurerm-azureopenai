f459efe6b02b7089fbe8a01041112b8d501d358a
soferreira <soferreira@microsoft.com>
2023-03-09 17:56:04 +0100

    bug fix

--- locals.tf
+++ locals.tf
@@ -9,6 +9,6 @@ locals {
     Application_Name = "var.application_name"
     Environment      = "var.environment"
   }
-  name = var.azureopenai_account_name != "" ? var.azureopenai_account_name : "openai-${random_integer.this.result}"
+  azureopenai_account_name = var.azureopenai_account_name != "" ? var.azureopenai_account_name : "openai-${random_integer.this.result}"
   location             = var.location != "" ? var.location : data.azurerm_resource_group.this.location
 }
