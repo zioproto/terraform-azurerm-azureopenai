e9a8fc58e1524b62f9a51f4ada39c4859688e6d7
soferreira <soferreira@microsoft.com>
2023-04-06 10:50:01 +0200

    fix variable description

--- variables.tf
+++ variables.tf
@@ -30,6 +30,7 @@ variable "default_tags_enabled" {
   type        = bool
   description = "Determines whether or not default tags are applied to resources. If set to true, tags will be applied. If set to false, tags will not be applied."
   default     = false
+  nullable = false
 }
 
 variable "deployment" {
@@ -43,7 +44,21 @@ variable "deployment" {
   }))
   default     = {}
   nullable    = false
-  description = "Parameters for deployment creation"
+  description = <<DESCRIPTION
+      deployment = {
+        name                 = "(Required) The name of the Cognitive Services Account Deployment. Changing this forces a new resource to be created."
+        cognitive_account_id = "(Required) The ID of the Cognitive Services Account. Changing this forces a new resource to be created."
+        model = {
+          model_format  = "(Required) The format of the Cognitive Services Account Deployment model. Changing this forces a new resource to be created. Possible value is OpenAI."
+          model_name    = "(Required) The name of the Cognitive Services Account Deployment model. Changing this forces a new resource to be created."
+          model_version = "(Required) The version of Cognitive Services Account Deployment model."
+        }
+        scale = {
+          scale_type = "(Required) Deployment scale type. Possible value is Standard. Changing this forces a new resource to be created."
+        }
+        rai_policy_name = "(Optional) The name of RAI policy. Changing this forces a new resource to be created."
+      }
+  DESCRIPTION
 }
 
 variable "environment" {
@@ -83,21 +98,7 @@ variable "private_endpoint" {
     private_service_connection_name = optional(string, "privateserviceconnection")
     is_manual_connection            = optional(bool, false)
   }))
-  description = <<DESCRIPTION
-    deployment = {
-      name                 = "(Required) The name of the Cognitive Services Account Deployment. Changing this forces a new resource to be created."
-      cognitive_account_id = "(Required) The ID of the Cognitive Services Account. Changing this forces a new resource to be created."
-      model = {
-        model_format  = "(Required) The format of the Cognitive Services Account Deployment model. Changing this forces a new resource to be created. Possible value is OpenAI."
-        model_name    = "(Required) The name of the Cognitive Services Account Deployment model. Changing this forces a new resource to be created."
-        model_version = "(Required) The version of Cognitive Services Account Deployment model."
-      }
-      scale = {
-        scale_type = "(Required) Deployment scale type. Possible value is Standard. Changing this forces a new resource to be created."
-      }
-      rai_policy_name = "(Optional) The name of RAI policy. Changing this forces a new resource to be created."
-    }
-  DESCRIPTION
+  description = "A map of objects that represent the configuration for a private endpoint."
   default     = {}
   nullable    = false
 }
